3a9c70f6958f97c21334b868abfcf5d785048cfa
Patrik Beka <patrik.beka@gmail.com>
2022-01-28 09:51:58 +0100

    Refactor dto

--- src/main/java/no/itera/assignment/dto/EmployeeDto.java
+++ src/main/java/no/itera/assignment/dto/EmployeeDto.java
@@ -1,64 +1,20 @@
 package no.itera.assignment.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class EmployeeDto {
-    private String name;
-    private int age;
-    private String departmentName;
-    private Instant startDate;
-    private Instant endDate;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public Instant getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Instant startDate) {
-        this.startDate = startDate;
-    }
-
-    public Instant getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Instant endDate) {
-        this.endDate = endDate;
-    }
-
-    @JsonIgnore
-    public boolean isActive() {
-        return endDate == null;
-    }
+  private String name;
+  private int age;
+  private String departmentName;
+  private Instant startDate;
+  private Instant endDate;
 }
